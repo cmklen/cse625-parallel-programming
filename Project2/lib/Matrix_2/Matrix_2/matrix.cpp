218941cc8e50be896c13d14c227b9443295f124f
cmklen <cmklen01@louisville.edu>
2022-10-19 12:34:37 -0400

    it ffucking works

--- Project2/lib/Matrix_2/Matrix_2/matrix.cpp
+++ Project2/lib/Matrix_2/Matrix_2/matrix.cpp
@@ -15,7 +15,7 @@
 // Assume matrices are stored in row-major linear array, and matrix indexing is 0-based.
 //
 //=====================================================================================
-void matrixMul_RowMajor(float* C, float* A, float* B, int RA, int CA, int CB)
+void matrixMul_RowMajor(float *C, float *A, float *B, int RA, int CA, int CB)
 {
 	// index of the C matrix element
 	int row, col;
@@ -44,7 +44,7 @@ void matrixMul_RowMajor(float* C, float* A, float* B, int RA, int CA, int CB)
 //
 //=========================================================================================
 
-void matrixMul_ColMajor(float* C, float* A, float* B, int RA, int CA, int CB)
+void matrixMul_ColMajor(float *C, float *A, float *B, int RA, int CA, int CB)
 {
 	// index of the C matrix element
 	int row, col;
@@ -59,7 +59,7 @@ void matrixMul_ColMajor(float* C, float* A, float* B, int RA, int CA, int CB)
 			for (int k = 0; k < CA; k++)
 				Cvalue += A[row + RA * k] * B[col * CA + k];
 
-			C[row + col * RA] = Cvalue;  // Note column-major !!
+			C[row + col * RA] = Cvalue; // Note column-major !!
 		}
 	}
 }
@@ -73,16 +73,17 @@ void matrixMul_ColMajor(float* C, float* A, float* B, int RA, int CA, int CB)
 // Assume matrices are stored in row-major linear array, and matrix indexing is 0-based.
 //
 //=====================================================================================
-void transpose (float* B, int RB, int CB)
+void transpose(float *B, int RB, int CB)
 {
-    int row, col;
-    float temp;
+	int row, col;
+	float temp;
 
-    // Transpose
+	// Transpose
 	for (row = 0; row < RB; row++)
 		for (col = 0; col < CB; col++)
 		{
-			if (row > col) continue;
+			if (row > col)
+				continue;
 
 			temp = B[row * CB + col];
 			B[row * CB + col] = B[col * CB + row];
@@ -90,13 +91,13 @@ void transpose (float* B, int RB, int CB)
 		}
 }
 
-void matrixMul_tmm(float* C, float* A, float* B, int RA, int CA, int CB)
+void matrixMul_tmm(float *C, float *A, float *B, int RA, int CA, int CB)
 {
 	// index of the C matrix element
 	int row, col;
 
 	// Transpose B matrix
-	transpose (B, CA, CB);
+	transpose(B, CA, CB);
 
 	// Matrix multiplication
 	for (row = 0; row < RA; ++row)
@@ -113,7 +114,6 @@ void matrixMul_tmm(float* C, float* A, float* B, int RA, int CA, int CB)
 			C[row * CB + col] = Cvalue;
 		}
 	}
-
 }
 
 //======================================================================================
@@ -121,55 +121,54 @@ void matrixMul_tmm(float* C, float* A, float* B, int RA, int CA, int CB)
 //=====================================================================================
 
 // Horizontal sum of 8 floats in __m256 X
-float hsum_avx (__m256 & X)
+float hsum_avx(__m256 &X)
 {
-    float temp[8], hsum = 0;
+	float temp[8], hsum = 0;
 
-    _mm256_storeu_ps(temp, X);
+	_mm256_storeu_ps(temp, X);
 
-    for (int i = 0; i < 8; i++)
-        hsum += temp[i];
+	for (int i = 0; i < 8; i++)
+		hsum += temp[i];
 
-    return hsum;
+	return hsum;
 }
 float hsum_avx_2(__m256 &X)
 {
-    float hsum = 0;
-    float *x = (float *)&X;
+	float hsum = 0;
+	float *x = (float *)&X;
 
-    for (int i = 0; i < 8; i++)
-        hsum += x[i];
+	for (int i = 0; i < 8; i++)
+		hsum += x[i];
 
-    return hsum;
+	return hsum;
 }
-
 
 // Horizontal sum of vectors (__mm128 float, mm256 float, and  __mm256 double)
-float hsum_ps_sse3(__m128 & v)
+float hsum_ps_sse3(__m128 &v)
 {
-	__m128 shuf = _mm_movehdup_ps(v);        // broadcast elements 3,1 to 2,0
+	__m128 shuf = _mm_movehdup_ps(v); // broadcast elements 3,1 to 2,0
 	__m128 sums = _mm_add_ps(v, shuf);
-	shuf = _mm_movehl_ps(shuf, sums);		// high half -> low half
+	shuf = _mm_movehl_ps(shuf, sums); // high half -> low half
 	sums = _mm_add_ss(sums, shuf);
-	return        _mm_cvtss_f32(sums);
+	return _mm_cvtss_f32(sums);
 }
 
-float hsum256_ps_avx(__m256 & v)
+float hsum256_ps_avx(__m256 &v)
 {
 	__m128 vlow = _mm256_castps256_ps128(v);
 	__m128 vhigh = _mm256_extractf128_ps(v, 1); // high 128
-	vlow = _mm_add_ps(vlow, vhigh);     // add the low 128
-	return hsum_ps_sse3(vlow);         // and inline the sse3 version, which is optimal for AVX
-	// (no wasted instructions, and all of them are the 4B minimum)
+	vlow = _mm_add_ps(vlow, vhigh);				// add the low 128
+	return hsum_ps_sse3(vlow);					// and inline the sse3 version, which is optimal for AVX
+												// (no wasted instructions, and all of them are the 4B minimum)
 }
 
-double hsum256_double_avx(__m256d & v)
+double hsum256_double_avx(__m256d &v)
 {
 	__m128d vlow = _mm256_castpd256_pd128(v);
-	__m128d vhigh = _mm256_extractf128_pd(v, 1);	// high 128
-	vlow = _mm_add_pd(vlow, vhigh);					// reduce down to 128
+	__m128d vhigh = _mm256_extractf128_pd(v, 1); // high 128
+	vlow = _mm_add_pd(vlow, vhigh);				 // reduce down to 128
 	__m128d high64 = _mm_unpackhi_pd(vlow, vlow);
-	return  _mm_cvtsd_f64(_mm_add_sd(vlow, high64));  // reduce to scalar
+	return _mm_cvtsd_f64(_mm_add_sd(vlow, high64)); // reduce to scalar
 }
 
 //======================================================================================
@@ -181,12 +180,12 @@ double hsum256_double_avx(__m256d & v)
 // Assume matrices are stored in row-major linear array, and matrix indexing is 0-based.
 //
 //=====================================================================================
-void matrixMul_AVX_tmm(float* C, float* A, float* B, int RA, int CA, int CB,
-	bool ToTranspose = true)
+void matrixMul_AVX_tmm(float *C, float *A, float *B, int RA, int CA, int CB,
+					   bool ToTranspose = true)
 {
 	if (ToTranspose)
-        // Transpose B matrix
-        transpose (B, CA, CB);
+		// Transpose B matrix
+		transpose(B, CA, CB);
 
 	for (int i = 0; i < RA; i++)
 	{
@@ -201,33 +200,33 @@ void matrixMul_AVX_tmm(float* C, float* A, float* B, int RA, int CA, int CB,
 			}
 
 			C[i * RA + j] = hsum256_ps_avx(X);
-			//C[i * RA + j] = hsum_avx(X);
-			//C[i * RA + j] = hsum_avx_2(X);
+			// C[i * RA + j] = hsum_avx(X);
+			// C[i * RA + j] = hsum_avx_2(X);
 		}
 	}
 }
 
-float AVXDot(float* A, float* B, int RA, int CA, int CB)
+float AVXDot(const std::vector<float> &v1, const std::vector<float> &v2)
 {
-    __m256 C = _mm256_setzero_ps();
-
-	for (int i = 0; i < RA; i+=8)
+	__m256 C = _mm256_setzero_ps();
+	size_t length = (v1.size() <= v2.size() ? v1.size() : v2.size());
+	float sum;
+	
+	for (int i = 0; i < length; i += 8)
 	{
 		__m256 X = _mm256_setzero_ps();
-		const __m256 mmA = _mm256_loadu_ps(A + i);
-		const __m256 mmB = _mm256_loadu_ps(B + i);
+		const __m256 mmA = _mm256_loadu_ps((float *)&v1[i]);
+		const __m256 mmB = _mm256_loadu_ps((float *)&v2[i]);
 		X = _mm256_mul_ps(mmA, mmB);
-		C[i/8] = hsum256_ps_avx(X);
-		std::cout << "Sub Sum:" << hsum256_ps_avx(X) << std::endl;
-		std::cout << "Cum Sum:" << hsum256_ps_avx(C) << std::endl;
+		sum += hsum256_ps_avx(X);
 	}
-	return hsum256_ps_avx(C);
+	return sum;
 }
 
 float SequentialDot(const std::vector<float> &v1, const std::vector<float> &v2)
 {
 	float result = 0;
- 	size_t length = (v1.size() <= v2.size() ? v1.size() : v2.size());
+	size_t length = (v1.size() <= v2.size() ? v1.size() : v2.size());
 	for (int i = 0; i < length; ++i)
 	{
 		result += v1[i] * v2[i];
